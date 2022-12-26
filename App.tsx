855b4bf3ea4303441e489f6685cfc2246233fac2
magnuschase <jakub@kapala.xyz>
2022-12-26 16:26:22 +0100

    feat: add tailwind's getColor function

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Provider } from 'react-redux'
 import TailwindProvider from './src/components/tailwind/TailwindProvider'
+import getColor from './src/helpers/getColor'
 import utilities from './tailwind.json'
 import HomeScreen from './src/screens/HomeScreen'
 import store from './src/store'
@@ -10,10 +11,18 @@ import store from './src/store'
 const Stack = createStackNavigator()
 
 export default function App() {
+	const NavigationTheme = {
+		...DefaultTheme,
+		colors: {
+			...DefaultTheme.colors,
+			primary: getColor('green', 500)
+		}
+	}
+
   return (
 		<TailwindProvider utilities={utilities}>
 			<Provider store={store}>
-				<NavigationContainer>
+				<NavigationContainer theme={NavigationTheme}>
 					<Stack.Navigator>
 						<Stack.Screen name="Home" component={HomeScreen} />
 					</Stack.Navigator>
