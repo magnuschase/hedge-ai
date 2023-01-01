ed6012b1f7f5e5d70c472a1b619e6d2fb2e67bdf
magnuschase <jakub@kapala.xyz>
2023-01-01 20:57:04 +0100

    feat: add SwitchCameraIcon component

--- src/components/icons/CameraIcon.tsx
+++ src/components/icons/CameraIcon.tsx
@@ -3,8 +3,9 @@ import Svg, { SvgProps, Path } from "react-native-svg"
 
 const CameraIcon = (props: SvgProps) => (
   <Svg
-    width={24}
-    height={24}
+		width={24}
+		height={24}
+		viewBox="0 0 24 24"
     fillRule="evenodd"
     clipRule="evenodd"
     {...props}
